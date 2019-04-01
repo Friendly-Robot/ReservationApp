864b4d66b0b66db7d4abd8117340985ba62ac340
Progoogler <andyzanli@gmail.com>
2019-04-01 13:50:27 -0700

    Commit theme provider for buttons, colors, and fonts in main export file of theme directory

---
+++ src/theme/colors.js
@@ -0,0 +1,6 @@
+export default {
+  PRIMARY_COLOR: 'rgb(30, 147, 242)',
+  SECONDARY_COLOR: 'rgb(238, 167, 2)',
+  WHITE_COLOR: 'rgb(255, 255, 255)',
+  DARK_COLOR: 'rgb(35, 40, 45)',
+}
---
+++ src/theme/index.js
@@ -0,0 +1,9 @@
+import buttons from './buttons';
+import colors from './colors';
+import fonts from './fonts';
+
+export default { 
+  ...buttons,
+  ...colors,
+  ...fonts,
+}

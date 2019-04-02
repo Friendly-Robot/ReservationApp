97578cb933b2234f899822f403b380ac3abfb9e6
Progoogler <andyzanli@gmail.com>
2019-04-01 17:30:54 -0700

    Include borderBox in export modules

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,9 +1,11 @@
-import buttons from './buttons';
+import borderBox from './borderBox';
 import colors from './colors';
+import constants from './constants';
 import fonts from './fonts';
 
 export default { 
-  ...buttons,
+  ...borderBox,
   ...colors,
+  ...constants,
   ...fonts,
 }

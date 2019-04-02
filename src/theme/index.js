bdebc50cff9ac25a13e62d0c00d7452afbeecacd
Progoogler <andyzanli@gmail.com>
2019-04-01 23:19:01 -0700

    Export hitSlop values

--- src/theme/index.js
+++ src/theme/index.js
@@ -3,6 +3,7 @@ import colors from './colors';
 import constants from './constants';
 import elevation from './elevation';
 import fonts from './fonts';
+import hitSlops from './hitSlops';
 
 export default { 
   ...borderBox,
@@ -10,4 +11,5 @@ export default {
   ...constants,
   elevation,
   ...fonts,
+  ...hitSlops,
 }

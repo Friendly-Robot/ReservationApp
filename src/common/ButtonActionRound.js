e2f5d50ff050f538b5905f0d0120405b44dff9c5
Progoogler <andyzanli@gmail.com>
2019-04-01 19:57:46 -0700

    Commit absolute round action button

---
+++ src/common/ButtonActionRound.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+import theme from '../theme';
+
+type buttonProps = {
+  onPress: Function,
+};
+
+export default ButtonActionRound = ({ onPress = () => {} }: buttonProps) => (
+  <View style={styles.container}>
+    <TouchableOpacity
+      activeOpacity={0.6}
+      onPress={onPress}
+      style={styles.button}  
+    >
+      <Text style={styles.icon}>+</Text>
+    </TouchableOpacity>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: theme.COLOR_PRIMARY,
+    borderRadius: theme.BORDER_RADIUS_ROUND,
+    ...theme.elevation,
+    height: 50,
+    justifyContent: 'center',
+    width: 50,
+
+  },
+  container: {
+    backgroundColor: 'transparent',
+    bottom: 25,
+    position: 'absolute',
+    right: 15,
+    zIndex: 10,
+  },
+  icon: {
+    color: theme.COLOR_WHITE,
+    fontSize: 35,
+  },
+});

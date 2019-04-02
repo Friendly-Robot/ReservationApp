26c0c3044d63629d067131db67345409cec39dc7
Progoogler <andyzanli@gmail.com>
2019-04-01 23:19:26 -0700

    Include COLOR_GREY in colors

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -1,6 +1,7 @@
 export default {
   COLOR_PRIMARY: 'rgb(30, 147, 242)',
   COLOR_SECONDARY: 'rgb(238, 167, 2)',
-  COLOR_WHITE: 'rgb(255, 255, 255)',
+  COLOR_GREY: 'rgb(211,211,211)',
   COLOR_DARK: 'rgb(35, 40, 45)',
+  COLOR_WHITE: 'rgb(255, 255, 255)',
 }

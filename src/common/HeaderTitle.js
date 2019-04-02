7f027c1c4728c6aabd0fdb05dffe234d3af78859
Progoogler <andyzanli@gmail.com>
2019-04-01 23:18:16 -0700

    Extend component to render button as well as title

--- src/common/HeaderTitle.js
+++ src/common/HeaderTitle.js
@@ -3,25 +3,42 @@
 */
 
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import theme from '../theme';
 
 type titleProps = {
+  buttonPress: Function,
+  buttonValue: string,
   title: string,
 };
 
-export default HeaderTitle = ({ title }) => (
+export default HeaderTitle = ({ buttonPress = () => {}, buttonValue = '', title = '' }) => (
   <View style={styles.container}>
+    {Boolean(buttonValue) &&
+      <TouchableOpacity
+        activeOpacity={0.8}
+        hitSlop={theme.HITSLOP_LARGE}
+        onPress={buttonPress}
+      >
+        <Text style={[styles.title, styles.buttonValue]}>{ buttonValue }</Text>
+      </TouchableOpacity>}
     <Text style={styles.title}>{ title }</Text>
   </View>
 );
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    marginLeft: theme.BORDER_PADDING_HORIZONTAL,
+  },
+  buttonValue: {
+    fontSize: 35,
+  },
   container: {
+    alignItems: 'center',
     backgroundColor: theme.COLOR_WHITE,
+    flexDirection: 'row',
     height: 65,
-    justifyContent: 'center',
     zIndex: 10,
   },
   title: {

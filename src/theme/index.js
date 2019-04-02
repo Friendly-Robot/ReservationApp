4a811d25a5e320557a3ccb3d86a8233e3ffdba24
Progoogler <andyzanli@gmail.com>
2019-04-01 19:57:03 -0700

    Include elevation is theme export

--- src/theme/index.js
+++ src/theme/index.js
@@ -1,11 +1,13 @@
 import borderBox from './borderBox';
 import colors from './colors';
 import constants from './constants';
+import elevation from './elevation';
 import fonts from './fonts';
 
 export default { 
   ...borderBox,
   ...colors,
   ...constants,
+  elevation,
   ...fonts,
 }

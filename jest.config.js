2dc1f4e3f164f6d16c0369e2c22d962a9d9273b2
Progoogler <andyzanli@gmail.com>
2019-04-02 00:56:03 -0700

    Commit jest config

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  verbose: true,
+  preset: "react-native",
+  cacheDirectory: "./cache",
+  coveragePathIgnorePatterns: [
+    "./app/utils/vendor"
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80
+    }
+  },
+  transformIgnorePatterns: [
+    "/node_modules/(?!react-native|react-clone-referenced-element|react-navigation)"
+  ],
+};

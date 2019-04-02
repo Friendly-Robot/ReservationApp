6a137472d82e0f43f5cc114f4314b9bda8d5469c
Progoogler <andyzanli@gmail.com>
2019-04-02 00:55:47 -0700

    Fix splice method and refactor isFirstDateEarlier() helper function

--- src/tabs/reservations/sortReservations.js
+++ src/tabs/reservations/sortReservations.js
@@ -16,7 +16,7 @@ class SortReservations {
         const currLength = groupByHotel[hotelName].length;
         while (i < currLength) {
           if (this.isFirstDateEarlier(res.arrivalDate, groupByHotel[hotelName][i].arrivalDate)) {
-            groupByHotel[hotelName].splice(i, reservation);
+            groupByHotel[hotelName].splice(i, 0, reservation);
             return;
           }
           i++;
@@ -33,10 +33,16 @@ class SortReservations {
   isFirstDateEarlier = (firstDate: string, secondDate: string) => {
     const splitFirstDate = firstDate.split('/');
     const splitSecondDate = secondDate.split('/');
-    if (parseInt(splitFirstDate[2], 10) < parseInt(splitSecondDate[2], 10)) return true;
-    if (parseInt(splitFirstDate[1], 10) < parseInt(splitSecondDate[1], 10)) return true;
-    if (parseInt(splitFirstDate[0], 10) < parseInt(splitSecondDate[0], 10)) return true;
-    return false;
+    const firstYear = parseInt(splitFirstDate[2], 10);
+    const secondYear = parseInt(splitSecondDate[2], 10);
+    if (firstYear > secondYear) return false;
+    const firstMonth = parseInt(splitFirstDate[0], 10);
+    const secondMonth = parseInt(splitSecondDate[0], 10);
+    if (firstMonth > secondMonth) return false;
+    const firstDay = parseInt(splitFirstDate[1], 10);
+    const secondDay = parseInt(splitSecondDate[1], 10);
+    if (firstMonth === secondMonth && firstDay > secondDay) return false;
+    return true;
   }
 }
 

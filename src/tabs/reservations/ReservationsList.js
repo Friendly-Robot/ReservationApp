4b5d6572fc42cc54c7f55281356a4b8cee114703
Progoogler <andyzanli@gmail.com>
2019-04-01 23:22:11 -0700

    Refactor component to render only the list component of reservations. Decoupled from Reservations.js.

--- src/tabs/reservations/ReservationsList.js
+++ src/tabs/reservations/ReservationsList.js
@@ -2,58 +2,24 @@
 * @flow
 */
 
-import React, { useEffect, useState } from 'react';
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
 
-import { graphql, ApolloProvider } from 'react-apollo';
-import gql from 'graphql-tag';
-
-import LoadingIndicator from '../../common/LoadingIndicator';
-import HeaderImage from '../../common/HeaderImage';
-import HeaderTitle from '../../common/HeaderTitle';
+import ReservationsHeader from './ReservationsHeader';
 import ReservationsListItem from './ReservationsListItem';
 import ButtonAction from '../../common/ButtonActionRound';
 
-import sortReservations from './sortReservations';
-
 import theme from '../../theme';
 
-const listDataValues = {
-  loading: boolean,
-  reservations: Array<T>,
-};
-
 type listProps = {
-  data: $Keys<typeof listDataValues/>,
+  hotels: Array<T>,
+  setShowScheduler: Function,
 };
 
-ReservationsList = ({ data }: listProps) => {
-  const { loading } = data;
-
-  if (loading) {
-    return (
-      <View style={styles.flex}>
-        { renderHeader('Reservations') }
-        <LoadingIndicator textLabel={'Loading reservations'} />
-      </View>
-    );
-  }
-
-  const [reservations, setReservations ] = useState([]);
-
-  useEffect(() => {
-    sortReservations(data.reservations, setReservations);
-  }, [data.reservations])
-
-  // TODO: Handle default view when loading has finished but reservations are empty.
-
-  return renderHotelReservations(reservations);
-}
-
-renderHotelReservations = (hotels) => (
+export default ReservationsList = ({hotels = [], setShowScheduler = () => {}}: listProps) => (
   <View style={styles.container}>
-    { renderHeader('Reservations') }
-    <ButtonAction onPress={() => {}} />
+    <ReservationsHeader title={'Reservations'} />
+    <ButtonAction onPress={() => setShowScheduler(true)} />
     <ScrollView contentContainerStyle={styles.scrollContainer}>
       {hotels.map(reservations => {
         if (reservations.length === 0) return null;
@@ -69,25 +35,9 @@ renderHotelReservations = (hotels) => (
   </View>
 );
 
-renderHeader = (title = 'Reservations') => (
-  <View style={styles.absolute}>
-    <HeaderTitle title={title} />
-    <HeaderImage />
-  </View>
-)
-
 const styles = StyleSheet.create({
-  absolute: {
-    left: 0,
-    position: 'absolute',
-    right: 0,
-    top: 0,
-  },
   container: {
     backgroundColor: theme.COLOR_WHITE,
-    flex: 1,
-  },
-  flex: {
     flex: 1,
   },
   scrollContainer: {
@@ -96,17 +46,3 @@ const styles = StyleSheet.create({
     marginTop: 215,
   }
 });
-
-// The `graphql` wrapper executes a GraphQL query and makes the results
-// available on the `data` prop of the wrapped component (ReservationsList here)
-export default graphql(gql`
-  query {
-    reservations {
-      id
-      name
-      hotelName
-      arrivalDate
-      departureDate
-    }
-  }
-`)(ReservationsList);

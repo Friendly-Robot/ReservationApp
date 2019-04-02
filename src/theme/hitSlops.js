4d8a3eede6b0d7eb407e20fb97be811556df8cfa
Progoogler <andyzanli@gmail.com>
2019-04-01 23:18:49 -0700

    Export hitSlop values

---
+++ src/theme/hitSlops.js
@@ -0,0 +1,5 @@
+export default {
+  HITSLOP_SMALL: { top: 5, right: 5, bottom: 5, left: 5 },
+  HITSLOP_MEDIUM: { top: 10, right: 10, bottom: 10, left: 10 },
+  HITSLOP_LARGE: { top: 15, right: 15, bottom: 15, left: 15 },
+}

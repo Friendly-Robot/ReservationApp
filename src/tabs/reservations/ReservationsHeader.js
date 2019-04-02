3f8391bb4a439e421f2b661fb5922761680ceaf3
Progoogler <andyzanli@gmail.com>
2019-04-01 23:31:44 -0700

    Commit separate Header component for reservations

---
+++ src/tabs/reservations/ReservationsHeader.js
@@ -0,0 +1,29 @@
+/*
+* @flow
+*/
+
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import HeaderImage from '../../common/HeaderImage';
+import HeaderTitle from '../../common/HeaderTitle';
+
+type props = {
+  title: string,
+};
+
+export default ReservationsHeader = ({title = 'Reservations'}: props) => (
+  <View style={styles.absolute}>
+    <HeaderTitle title={title} />
+    <HeaderImage />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  absolute: {
+    left: 0,
+    position: 'absolute',
+    right: 0,
+    top: 0,
+  },
+});

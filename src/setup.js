ce449d54ff61d5e2e1f7392d924b65901a8fa91b
Progoogler <andyzanli@gmail.com>
2019-04-01 17:27:10 -0700

    Setup apollo provider

--- src/setup.js
+++ src/setup.js
@@ -7,13 +7,25 @@
  */
 
 import React, {Component} from 'react';
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-boost';
 import App from './App';
+import env from './env';
 
 type Props = {};
 export default class Root extends Component<Props> {
+  constructor(...args) {
+    super(...args);
+    this.client = new ApolloClient({
+      uri: env.graphqlURL,
+    });
+  }
+
   render() {
     return (
-      <App />
+      <ApolloProvider client={this.client}>
+        <App />
+      </ApolloProvider>
     );
   }
 }

df1bec0f2e7b55481d1191ed68e6f4045cb3ec78
Progoogler <andyzanli@gmail.com>
2019-04-02 17:03:08 -0700

    Convert entry app to PureComponent

--- src/setup.js
+++ src/setup.js
@@ -6,21 +6,20 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
 import App from './App';
 import env from './env';
 
 type Props = {};
-export default class Root extends Component<Props> {
-  constructor(...args) {
-    super(...args);
+export default class Root extends PureComponent<Props> {
+  constructor() {
+    super();
     this.client = new ApolloClient({
       uri: env.graphqlURL,
     });
   }
-
   render() {
     return (
       <ApolloProvider client={this.client}>
